31f6d93db4366ec06eebea42c4ab0ad95be419cb
elnathanau1 <elnathanau1@gmail.com>
2019-05-22 12:01:59 -0700

    remove ds_store

--- boi_site/boi_stats/models.py
+++ boi_site/boi_stats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(primary_key = True, max_length = 50)
+
+    class Meta:
+        db_table = 'users'
+        managed = True
+
+    def __str__(self):
+        return (self.name)
